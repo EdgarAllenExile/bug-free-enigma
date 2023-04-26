b8aa88ca9c13d39c734f2417aedf9ec12c7bc825
EdgarAllenExile <riley.mercer4@gmail.com>
2023-04-27 00:06:06 +0800

    Light polish - To Add: filter by ingrdients / self-host / seed data

--- recipebook/db/migrate/20230424024655_create_recipes.rb
+++ recipebook/db/migrate/20230424024655_create_recipes.rb
@@ -1,14 +1,14 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.text :title
+      t.text :title, :default => "Not Specified"
       t.integer :user_id
-      t.text :category
-      t.text :cusine
-      t.text :dietary
-      t.integer :cook_time
-      t.integer :difficulty
-      t.integer :serves
+      t.text :category, :default => "Not Specified"
+      t.text :cusine, :default => "Not Specified"
+      t.text :dietary, :default => "Not Specified"
+      t.integer :cook_time, :default => 10
+      t.integer :difficulty, :default => 1
+      t.integer :serves, :default => 1
       t.text :image
       t.timestamps
     end
--- recipebook/db/migrate/20230424024714_create_steps.rb
+++ recipebook/db/migrate/20230424024714_create_steps.rb
@@ -2,7 +2,7 @@ class CreateSteps < ActiveRecord::Migration[7.0]
   def change
     create_table :steps do |t|
       t.integer :step_no
-      t.text :process
+      t.text :process, :default => "Not Specified"
       t.timestamps
     end
   end
